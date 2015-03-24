d2c371c4eb55ce1104f13cff105e8c9b8ad745f3
Andrew Bailey <public7727@gmail.com>
2015-03-24 11:04:05 -0400

    Added additional node modules.

--- v2/gulpfile.js
+++ v2/gulpfile.js
@@ -3,12 +3,15 @@ var plugins = require('gulp-load-plugins')();
 
 gulp.task('sass', function () {
     gulp.src('scss/style.scss')
-    .pipe(plugins.rubySass())
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sass())
+    .pipe(plugins.autoprefixer('last 3 versions'))
     .pipe(plugins.minifyCss())
+    .pipe(plugins.sourcemaps.write())
     .pipe(gulp.dest('css'))
     .pipe(plugins.livereload({start: true}));
 });
 
 gulp.task('default', ['sass'], function (){
-    gulp.watch('**/*.{scss,html,js}', ['sass']);
+    gulp.watch(['**/*.{scss}'], ['sass']);
 });
