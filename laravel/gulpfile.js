bf00b22a2870b51552e0a783afd83b51f42317dc
Andrew Bailey <public7727@gmail.com>
2015-03-30 14:51:32 -0400

    Init commit for conversion of andrewbailey.me to Laravel framework

---
+++ laravel/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+gulp.task('sass', function () {
+    gulp.src('public/scss/style.scss')
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sass())
+    .pipe(plugins.autoprefixer('last 3 versions'))
+    .pipe(plugins.minifyCss())
+    .pipe(plugins.sourcemaps.write('.'))
+    .pipe(gulp.dest('public/css'))
+    .pipe(plugins.livereload({start: true}));
+});
+
+gulp.task('default', ['sass'], function (){
+    gulp.watch(['public/scss/**/*.scss', 'resources/views/*.php'], ['sass']);
+});
